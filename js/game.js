0830b8f039c1fff0b208ddd50dc8b5e4b9049403
ouyang <243175186@qq.com>
2020-01-02 15:11:52 +0800

    通过方向键控制蛇头的方向

--- js/game.js
+++ js/game.js
@@ -15,17 +15,53 @@
     this.food.render(this.map)
 
     autoMove()
+
+    // 根据按键调整蛇头的方向
+    bindKey()
   }
 
   // 让蛇自动移动
   function autoMove () {
     // 这里需要注意this的指向问题，不用bind改变指向的话，this在setInterval中是指向window的
-    setInterval(function () {
+    let timeId = setInterval(function () {
       this.snake.move()
       this.snake.render(this.map)
+
+      // 判断蛇是否撞墙了，也就是蛇头到达地图边界
+      let snakeHeadX = this.snake.body[0].x * this.snake.width
+      let snakeHeadY = this.snake.body[0].y * this.snake.height
+      if (snakeHeadX >= this.map.offsetWidth || snakeHeadY >= this.map.offsetHeight) {
+        alert('Game over!')
+        clearInterval(timeId)
+      }
+      if (snakeHeadX < 0 || snakeHeadY < 0) {
+        alert('Game over!')
+        clearInterval(timeId)
+      }
     }.bind(that), 300);
   }
   
+  // 获取按键
+  function bindKey () {
+    document.addEventListener('keydown', function (e) {
+      e = e || window.event
+      switch (e.keyCode) {
+        case 37:
+          this.snake.direction = 'left'
+          break
+        case 38:
+          this.snake.direction = 'top'
+          break
+        case 39:
+          this.snake.direction = 'right'
+          break
+        case 40:
+          this.snake.direction = 'bottom'
+          break
+
+      }
+    }.bind(that))
+  }
 
   window.Game = Game
 }(window))
